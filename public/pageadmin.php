5cca336eba9d581240113849a1ad0ecd1299a1d0
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-01-17 12:58:10 +0100

    added logout on pageadmin

--- public/pageadmin.php
+++ public/pageadmin.php
@@ -20,8 +20,8 @@ require_once __DIR__ . '/admin/check_admin.php';
         <a class="active" href="index.php">Accueil</a>
         <a href="reserv.php">Réserver ma Chambre</a>
         <a href="listechambre.php">Liste des Chambres</a>
-        <a href="#inscr">M'inscrire</a>
         <a href="contact.php">Nous Contacter</a>
+        <a href ="?action=logout"> Me déconnecter</a>
       </div>
       <div class="droite">
         <a href="s'identifier.php"><img src="images/icone.png" alt="identifant"></a>
