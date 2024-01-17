985d43869a60a5cdf65450836aac13cb1145df5c
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-01-17 12:38:21 +0100

    creation admin pages

--- public/admin/pageadmin.php
+++ public/admin/pageadmin.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../activate_session.php';
 require_once __DIR__ . '/../logout.php';
-require_once __DIR__ . '/check_admin.php';
+require_once __DIR__ . '/../admin/check_admin.php';
 
 ?>
 <!DOCTYPE html>
--- public/login.php
+++ public/login.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../activate_session.php';
 require_once __DIR__ . '/../database.php';
-require_once __DIR__ . '/check_admin.php';
+require_once __DIR__ . '/../admin/check_admin.php';
 
 if (isset($_SESSION['user'])) {
     header('Location:/index.php');
