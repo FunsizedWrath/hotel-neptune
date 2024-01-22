7ee0a4a91baa01aca256042df1a7c8be225eb88c
Naimau-del <147485179+Naimau-del@users.noreply.github.com>
2024-01-22 12:54:15 +0100

    Update booking_managment.php

--- public/admin/booking_managment.php
+++ public/admin/booking_managment.php
@@ -1,8 +1,55 @@
 <?php
 
-require_once __DIR__ . '/../../activate_session.php';
-require_once __DIR__ . '/check_admin.php';
-require_once __DIR__ . '/../../database.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../../activate_session.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'check_admin.php';
+
+$request = $database->prepare('SELECT * FROM reservation r');
+$request->execute();
+
+if (empty($reservations = $request->fetchAll())) {
+    echo("Vous n'avez aucune reservation.");
+}
+
+usort($reservations, function ($a, $b) {
+  return $a['date_arrivee'] - $b['date_arrivee'];
+});
 
 ?>
+<!DOCTYPE html>
+<html lang="fr">
 
+<head>
+    <link rel="stylesheet" href="styles/pageadmin.css" />
+    <link rel="stylesheet" href="styles/rooms.css" />
+    <title>  Neptune Hotel  </title>
+    <link rel="shortcut icon" href="images/Logo-Neptune-avec-rond-e1607450857665.ico" />
+</head>
+
+<body>
+    <?php require_once '../navbar.php' ?>
+  <br>
+  <br>
+  <br>
+    <div class="titre">
+      Gestion des reservations
+  </div>
+
+    <div class="room-list">
+    <?php foreach($reservations as $reservation) { ?>
+      <div class="room">
+          <div class="desc">
+              <b>date_arrivee : <?php echo $reservation['date_arrivee'] ?></b>
+              <br>
+              <b>date_depart : <?php echo $reservation['date_depart'] ?></b>
+            </div>
+            <a href=<?php echo "booking_validation.php?id=" . $reservation["id"] ?>><button> Valider</button></a>
+        </div>
+    <?php } ?>
+    </div>
+</body>
+
+
+
+
+
+</html>
